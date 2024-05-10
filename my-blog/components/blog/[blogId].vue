590b6b0210ad042abe60ad21a3799de0d05b57df
YeXing_0901 <13319139772@163.com>
2024-05-11 04:15:18 +0800

    feat:新增记录文章浏览位置

--- my-blog/components/blog/[blogId].vue
+++ my-blog/components/blog/[blogId].vue
@@ -41,6 +41,15 @@ let content = ref();
 let title = ref();
 let updateTime = ref();
 let tags = ref();
+
+// 记录滚动条的位置
+const scrollPosition = ref(0);
+
+const handleScroll = () => {
+  scrollPosition.value = window.scrollY;
+  localStorage.setItem("scrollPosition", scrollPosition.value);
+};
+
 onMounted(async () => {
   // 根据文章的ID获取文章内容
   const res = await $fetch(`/webapi/article/${blogId}`);
@@ -49,14 +58,32 @@ onMounted(async () => {
   title.value = res.data.title;
   updateTime.value = res.data.updateTime;
   tags.value = res.data.tags;
+
+  // 监听滚动事件
+  window.addEventListener("scroll", handleScroll);
+  // 获取本地存储的滚动条位置
+  scrollPosition.value = localStorage.getItem("scrollPosition");
+  // 如果本地存储的滚动条位置不为空，则滚动到指定位置
+  if (scrollPosition.value) {
+    // 在浏览器重绘下一帧前执行
+    requestAnimationFrame(() => {
+      window.scrollTo(0, parseInt(scrollPosition.value));
+    });
+  }
 });
 
+onBeforeUnmount(() => {
+  // 组建销毁前移除监听事件,防止监听事件重复执行改变记录的滚动条位置
+  window.removeEventListener("scroll", handleScroll);
+});
+
+// seo
 useHead({
   /* 
   如果直接写title:title.value的话,无法自动更新
   用()=>title.value的话,可以动态的更新标题
   */
-  title: ()=>title.value,
+  title: () => title.value,
 });
 </script>
 
